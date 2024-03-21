4520132892e812bd6e65f9daefa4bc0ec2a6210a
Rani Kumari <rkcrj100@gmail.com>
2024-03-21 17:04:36 +0530

    final

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
-    title: "Who I ammmmmm",
+    title: "Who I am",
     description1: "Hello there! I'm Rani Kumari, a third-year B.Tech student pursuing Electronics and Communication Engineering. My passion for coding has led me to become proficient in a diverse array of programming languages such as C, C++, Java, Python, javaScript ,  Web developer, App developer . I have also dabbled in app development, showcasing my versatility in the world of software creation. I am a compititive programer as well.",
     description2:"Beyond my academic journey, I have actively taken up leadership roles within various clubs on campus, proudly serving as the head of different clubs. One of my most significant achievements is being appointed as the Beta Microsoft Learn Student Ambassador (MLSA). In this role, I get the fantastic opportunity to share my love for technology with others and contribute to the growth of the developer community.",
     image: 2
